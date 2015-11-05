7c1778bb04fac7713462259bf8a47365f8977359
supeindesu <supeindesu@hotmail.com>
2015-11-05 19:30:10 +0100

    Update hangperson_game.rb

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -17,6 +17,57 @@ class HangpersonGame
     @guesses = ''
     @wrong_guesses = ''
   end
+  
+  def guess(letter)
+    case letter
+      when nil
+        raise ArgumentError
+      when ""
+        raise ArgumentError
+      when /[^a-zA-Z]/
+	      raise ArgumentError
+      else
+        if @word.include? letter.downcase
+          if @guesses.include? letter.downcase
+            false
+          else
+            @guesses += letter.downcase
+            true
+          end
+        else
+          if @wrong_guesses.include? letter.downcase
+            false
+          else
+            @wrong_guesses += letter.downcase
+            true
+          end
+        end
+    end
+  end
+  
+  def word_with_guesses()
+    difference = (@word.split(//)-@guesses.split(//)).uniq
+    output = ""
+    @word.split(//).each do |letter|
+      if ! difference.include? letter
+        output += letter
+      else 
+        output += "-"
+      end
+    end
+    output
+  end
+
+  def check_win_or_lose()
+    difference = (@word.split(//)-@guesses.split(//)).uniq
+    if difference.empty?
+      :win
+    elsif @wrong_guesses.length > 6
+      :lose
+    else 
+      :play
+    end
+  end
 
   def self.get_random_word
     require 'uri'
